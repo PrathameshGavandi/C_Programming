2c47b514e08110674aa7e9e7308decca83c7cdc2
Prathamesh_Gavandi <115519891+PrathameshGavandi@users.noreply.github.com>
2025-11-16 14:48:33 +0530

    Add files via upload

---
+++ Classwork/program132.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+float Average(int Arr[], int iSize)
+{
+    int iCnt = 0, iSum = 0, iAvg = 0;
+
+    for (iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        iSum = iSum + Arr[iCnt];
+    }
+
+    return ((float)iSum / (float)iSize);        // Typecasting
+
+}
+
+int main()
+{
+    int iLength = 0, iCnt = 0; 
+    float fRet = 0;
+    int *ptr = NULL;
+    
+
+    printf("Enter number of elements : \n");
+    scanf("%d", &iLength);
+
+    ptr = (int *)malloc(iLength * sizeof(int));
+    if(NULL == ptr)      
+    {
+        printf("Unable to allocate momory\n");
+        return -1;
+    }
+
+    printf("Enter the elements : \n");
+    for(iCnt=0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d", &ptr[iCnt]);
+    }
+
+    fRet = Average(ptr, iLength);
+    
+    printf("Average of elements is : %f\n", fRet);
+
+    free(ptr);
+
+    return 0;
+}
