49f99792ceafc594e458d7d5ad67fe5d6f5eda3a
Prathamesh_Gavandi <115519891+PrathameshGavandi@users.noreply.github.com>
2025-12-05 14:21:50 +0530

    Add files via upload

---
+++ Assignments/Assignment_35/program35_4.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+BOOL Difference(char str[])
+{
+    while (*str != '\0')
+    {
+        if(*str=='a' || *str=='e' || *str=='i' || *str=='o' || *str=='u' ||
+            *str=='A' || *str=='E' || *str=='I' || *str=='O' || *str=='U')
+            {
+
+                return 1;
+            }
+         
+            *str++;
+
+    }
+
+    return FALSE;
+    
+
+    
+}
+
+int main()
+{
+    char arr[20];;
+    BOOL bRet = FALSE;
+
+    printf("Enter string : \n");
+    scanf("%[^'\n']s",arr);
+    
+    bRet = Difference(arr);
+    if(bRet == TRUE)
+    {
+        printf("Contains Vowel");
+    }
+    else
+    {
+        printf("There is no Vowel");
+    }
+
+    
+    return 0;
+}
